e4467c1d19b77188721fe583a420a9e382c19fe1
Egga <57639719+eggstrema@users.noreply.github.com>
2022-03-31 21:57:14 +0200

    add ID

--- src/main/java/de/egga/designamusement/mainstage/Show.java
+++ src/main/java/de/egga/designamusement/mainstage/Show.java
@@ -1,16 +1,34 @@
 package de.egga.designamusement.mainstage;
 
 import java.time.LocalTime;
+import java.util.UUID;
 
 public class Show {
 
+    private final UUID id;
     private final int price;
 
-    public Show(String title, LocalTime afternoon, int price) {
+    public Show(UUID id, String title, LocalTime afternoon, int price) {
+        this.id = id;
         this.price = price;
     }
 
     public int getPrice() {
         return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Show show = (Show) o;
+
+        return id.equals(show.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
     }
 }
--- src/test/java/de/egga/designamusement/mainstage/MainStageTest.java
+++ src/test/java/de/egga/designamusement/mainstage/MainStageTest.java
@@ -8,9 +8,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalTime;
 import java.util.List;
+import java.util.UUID;
 
 import static java.time.LocalTime.parse;
 import static java.time.format.DateTimeFormatter.ISO_TIME;
+import static java.util.UUID.randomUUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -21,6 +23,7 @@ public class MainStageTest {
     public static final LocalTime ANY_TIME = parse("16:00:00", ISO_TIME);
     public static final String ANY_TITLE = "Julia & Oscar the Grouch";
     public static final int ANY_PRICE = 20;
+    public static final UUID ANY_UUID = randomUUID();
 
     @Mock
     MainStageRepository repository;
@@ -30,30 +33,30 @@ public class MainStageTest {
 
     @Test
     void new_show_is_contained_in_list_of_all_shows() {
-        Show newShow = new Show(ANY_TITLE, ANY_TIME, ANY_PRICE);
+        Show newShow = new Show(ANY_UUID, ANY_TITLE, ANY_TIME, ANY_PRICE);
         stage.add(newShow);
         assertThat(stage.listAllShows()).contains(newShow);
     }
 
     @Test
     void new_show_is_persisted_in_repository() {
-        Show newShow = new Show(ANY_TITLE, ANY_TIME, ANY_PRICE);
+        Show newShow = new Show(ANY_UUID, ANY_TITLE, ANY_TIME, ANY_PRICE);
         stage.add(newShow);
         verify(repository).save(newShow);
     }
 
     @Test
     void list_all_shows_fetches_updates_from_repository() {
-        Show show = new Show(ANY_TITLE, ANY_TIME, ANY_PRICE);
+        Show show = new Show(ANY_UUID, ANY_TITLE, ANY_TIME, ANY_PRICE);
         when(repository.receive()).thenReturn(List.of(show));
         assertThat(stage.listAllShows()).contains(show);
     }
 
     @Test
     void too_expensive_shows_are_filtered_out() {
-        Show anyShow = new Show(ANY_TITLE, ANY_TIME, ANY_PRICE);
-        Show cheapShow = new Show(ANY_TITLE, ANY_TIME, 2);
-        Show expensiveShow = new Show(ANY_TITLE, ANY_TIME, 200);
+        Show anyShow = new Show(ANY_UUID, ANY_TITLE, ANY_TIME, ANY_PRICE);
+        Show cheapShow = new Show(randomUUID(), ANY_TITLE, ANY_TIME, 2);
+        Show expensiveShow = new Show(randomUUID(), ANY_TITLE, ANY_TIME, 200);
         when(repository.receive()).thenReturn(List.of(anyShow, cheapShow, expensiveShow));
         assertThat(stage.listShowsBelow(50)).containsOnly(anyShow, cheapShow);
     }
