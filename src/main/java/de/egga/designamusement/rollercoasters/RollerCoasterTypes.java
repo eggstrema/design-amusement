5d5b7cf39b0ecdea974aec2c076fde6c5382dad4
Egga <57639719+eggstrema@users.noreply.github.com>
2022-04-01 19:30:13 +0200

    add some rollercoasters

--- src/main/java/de/egga/designamusement/rollercoasters/RollerCoasterTypes.java
+++ src/main/java/de/egga/designamusement/rollercoasters/RollerCoasterTypes.java
@@ -1,5 +1,8 @@
 package de.egga.designamusement.rollercoasters;
 
 public enum RollerCoasterTypes {
-    COUNT_VON_ROUND
+    COUNT_VON_ROUND,
+    POCO_LOCO,
+    TWIDDLE_BUGS,
+    LEO_THE_PARTY_GLIDER
 }
