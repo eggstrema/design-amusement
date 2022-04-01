b71e9ce27fde3f3c966d4fea421bd4f293d51890
Egga <57639719+eggstrema@users.noreply.github.com>
2022-04-01 19:33:56 +0200

    organize imports

--- src/main/java/de/egga/designamusement/shops/nonfood/NonFoodShop.java
+++ src/main/java/de/egga/designamusement/shops/nonfood/NonFoodShop.java
@@ -3,9 +3,6 @@ package de.egga.designamusement.shops.nonfood;
 import de.egga.designamusement.shops.Location;
 import de.egga.designamusement.shops.Shop;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class NonFoodShop extends Shop {
 
 
