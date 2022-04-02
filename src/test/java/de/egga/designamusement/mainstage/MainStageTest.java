0753f7cce4b1b020e64c4f610f7b1f511ecc2023
Egga <57639719+eggstrema@users.noreply.github.com>
2022-04-02 21:06:53 +0200

    put test data provider in authoritative place

--- src/test/java/de/egga/designamusement/mainstage/MainStageTest.java
+++ src/test/java/de/egga/designamusement/mainstage/MainStageTest.java
@@ -10,11 +10,8 @@ import java.util.List;
 
 import static de.egga.designamusement.mainstage.Audience.*;
 import static de.egga.designamusement.mainstage.ShowTest.*;
-import static de.egga.designamusement.mainstage.ShowTest.ANY_TIME;
-import static de.egga.designamusement.mainstage.ShowTest.ANY_TITLE;
 import static java.time.LocalTime.parse;
 import static java.time.format.DateTimeFormatter.ISO_TIME;
-import static java.util.UUID.randomUUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -33,48 +30,48 @@ public class MainStageTest {
 
     @Test
     void new_show_is_contained_in_list_of_all_shows() {
-        Show newShow = new Show(randomUUID(), ANY_TITLE, ANY_TIME, ANY_PRICE, ANY_AUDIENCE);
+        Show newShow = anyShow();
         stage.add(newShow);
         assertThat(stage.listAllShows()).contains(newShow);
     }
 
     @Test
     void new_show_is_persisted_in_repository() {
-        Show newShow = new Show(randomUUID(), ANY_TITLE, ANY_TIME, ANY_PRICE, ANY_AUDIENCE);
+        Show newShow = anyShow();
         stage.add(newShow);
         verify(repository).save(newShow);
     }
 
     @Test
     void list_all_shows_fetches_updates_from_repository() {
-        Show show = new Show(randomUUID(), ANY_TITLE, ANY_TIME, ANY_PRICE, ANY_AUDIENCE);
+        Show show = anyShow();
         when(repository.receive()).thenReturn(List.of(show));
         assertThat(stage.listAllShows()).contains(show);
     }
 
     @Test
     void too_expensive_shows_are_filtered_out() {
-        Show regularShow = new Show(randomUUID(), ANY_TITLE, ANY_TIME, 20, ANY_AUDIENCE);
-        Show cheapShow = new Show(randomUUID(), ANY_TITLE, ANY_TIME, 2, ANY_AUDIENCE);
-        Show expensiveShow = new Show(randomUUID(), ANY_TITLE, ANY_TIME, 200, ANY_AUDIENCE);
+        Show regularShow = showFor(20);
+        Show cheapShow = showFor(2);
+        Show expensiveShow = showFor(200);
         when(repository.receive()).thenReturn(List.of(regularShow, cheapShow, expensiveShow));
         assertThat(stage.listShowsBelow(50)).containsOnly(regularShow, cheapShow);
     }
 
     @Test
     void not_suitable_shows_are_filtered_out() {
-        Show familyShow = new Show(randomUUID(), ANY_TITLE, ANY_TIME, ANY_PRICE, FAMILIES_WITH_CHILDREN);
-        Show explicitShow = new Show(randomUUID(), ANY_TITLE, ANY_TIME, ANY_PRICE, EXPLICIT_CONTENT);
-        Show fanShow = new Show(randomUUID(), ANY_TITLE, ANY_TIME, ANY_PRICE, WHOVIANS);
+        Show familyShow = showFor(FAMILIES_WITH_CHILDREN);
+        Show explicitShow = showFor(EXPLICIT_CONTENT);
+        Show fanShow = showFor(WHOVIANS);
         when(repository.receive()).thenReturn(List.of(familyShow, explicitShow, fanShow));
         assertThat(stage.listShowsFor(WHOVIANS)).containsOnly(fanShow);
     }
 
     @Test
     void returns_morning_show_in_the_morning() {
-        Show morningShow = new Show(randomUUID(), ANY_TITLE, parse("10:00:00", ISO_TIME), ANY_PRICE, ANY_AUDIENCE);
-        Show noonShow = new Show(randomUUID(), ANY_TITLE, parse("13:00:00", ISO_TIME), ANY_PRICE, ANY_AUDIENCE);
-        Show primeTimeShow = new Show(randomUUID(), ANY_TITLE, parse("20:00:00", ISO_TIME), ANY_PRICE, ANY_AUDIENCE);
+        Show morningShow = showAt("10:00:00");
+        Show noonShow = showAt("13:00:00");
+        Show primeTimeShow = showAt("20:00:00");
         when(repository.receive()).thenReturn(List.of(morningShow, noonShow, primeTimeShow));
 
         when(clock.now()).thenReturn(parse("08:00:00", ISO_TIME));
@@ -84,9 +81,9 @@ public class MainStageTest {
 
     @Test
     void returns_noon_show_later() {
-        Show morningShow = new Show(randomUUID(), ANY_TITLE, parse("10:00:00", ISO_TIME), ANY_PRICE, ANY_AUDIENCE);
-        Show noonShow = new Show(randomUUID(), ANY_TITLE, parse("13:00:00", ISO_TIME), ANY_PRICE, ANY_AUDIENCE);
-        Show primeTimeShow = new Show(randomUUID(), ANY_TITLE, parse("20:00:00", ISO_TIME), ANY_PRICE, ANY_AUDIENCE);
+        Show morningShow = showAt("10:00:00");
+        Show noonShow = showAt("13:00:00");
+        Show primeTimeShow = showAt("20:00:00");
         when(repository.receive()).thenReturn(List.of(morningShow, noonShow, primeTimeShow));
 
         when(clock.now()).thenReturn(parse("12:55:00", ISO_TIME));
@@ -96,14 +93,15 @@ public class MainStageTest {
 
     @Test
     void returns_prime_time_show_in_the_evening() {
-        Show morningShow = new Show(randomUUID(), ANY_TITLE, parse("10:00:00", ISO_TIME), ANY_PRICE, ANY_AUDIENCE);
-        Show noonShow = new Show(randomUUID(), ANY_TITLE, parse("13:00:00", ISO_TIME), ANY_PRICE, ANY_AUDIENCE);
-        Show primeTimeShow = new Show(randomUUID(), ANY_TITLE, parse("20:00:00", ISO_TIME), ANY_PRICE, ANY_AUDIENCE);
+        Show morningShow = showAt("10:00:00");
+        Show noonShow = showAt("13:00:00");
+        Show primeTimeShow = showAt("20:00:00");
         when(repository.receive()).thenReturn(List.of(morningShow, noonShow, primeTimeShow));
 
         when(clock.now()).thenReturn(parse("18:00:00", ISO_TIME));
 
         assertThat(stage.getNextShow()).isEqualTo(primeTimeShow);
     }
+
 
 }
--- src/test/java/de/egga/designamusement/mainstage/ShowTest.java
+++ src/test/java/de/egga/designamusement/mainstage/ShowTest.java
@@ -5,6 +5,7 @@ import java.time.LocalTime;
 import static de.egga.designamusement.mainstage.Audience.WHOVIANS;
 import static java.time.LocalTime.parse;
 import static java.time.format.DateTimeFormatter.ISO_TIME;
+import static java.util.UUID.randomUUID;
 
 public class ShowTest {
 
@@ -13,4 +14,19 @@ public class ShowTest {
     public static final int ANY_PRICE = 20;
     public static final Audience ANY_AUDIENCE = WHOVIANS;
 
+     public static Show anyShow() {
+        return new Show(randomUUID(), ANY_TITLE, ANY_TIME, ANY_PRICE, ANY_AUDIENCE);
+    }
+
+     public static Show showAt(String time) {
+        return new Show(randomUUID(), ANY_TITLE, parse(time, ISO_TIME), ANY_PRICE, ANY_AUDIENCE);
+    }
+
+     public static Show showFor(int price) {
+        return new Show(randomUUID(), ANY_TITLE, ANY_TIME, price, ANY_AUDIENCE);
+    }
+
+     public static Show showFor(Audience audience) {
+        return new Show(randomUUID(), ANY_TITLE, ANY_TIME, ANY_PRICE, audience);
+    }
 }
